3400255882bc002ea9b72edcfb24ca25f2edddc2
Jhory Toro <jhordytoro49@gmail.com>
2022-03-30 09:49:57 -0500

    'Models'

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+#python
+from datetime import date
+from uuid import UUID
+from typing import Optional
+
+#pydantic
+from pydantic import BaseModel
+from pydantic import Field,EmailStr
+
+#FastAPI
 from fastapi import FastAPI
+
+class user_base(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+    
+class user_login(user_base):
+    password: str = Field(...,min_length=8)
+    
+class user(user_base):
+    first_name: str = Field(...,min_length=1,max_length=50)
+    last_name: str = Field(...,min_length=1,max_length=50)
+    birt_date: Optional[date] = Field(default=None)
+
+class twitter(BaseModel):
+    pass
 
 app = FastAPI()
 
