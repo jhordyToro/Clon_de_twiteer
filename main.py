260d5e9550d964a0fd9ff08d8d745385174fb4a8
Jhory Toro <jhordytoro49@gmail.com>
2022-03-30 10:39:41 -0500

    'path_operation_extructure_user'

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #python
 from datetime import date, datetime
 from uuid import UUID
-from typing import Optional
+from typing import Optional, List
 
 #pydantic
 from pydantic import BaseModel
@@ -9,7 +9,7 @@ from pydantic import Field,EmailStr
 
 #FastAPI
 from fastapi import FastAPI
-
+from fastapi import status
 
 class User_Base(BaseModel):
     user_id: UUID = Field(...)
@@ -35,6 +35,72 @@ class twitter(BaseModel):
 
 app = FastAPI()
 
-@app.get('/')
+#Path parameters 
+##Home
+@app.get('/',tags=['Home'])
 def home():
     return {'home': 'welcome to home'}
+
+##Users
+@app.post(
+    path='/singnup',
+    status_code=status.HTTP_201_CREATED,
+    response_model=User,
+    summary='create_user',
+    tags=['Users']
+)
+def signup():
+    pass
+
+@app.post(
+    path='/login',
+    status_code=status.HTTP_200_OK,
+    response_model=User,
+    summary='Login_user',
+    tags=['Users']
+)
+def Login():
+    pass
+
+@app.get(
+    path='/users',
+    status_code=status.HTTP_200_OK,
+    response_model=List[User],
+    summary='show_all_user',
+    tags=['Users']
+)
+def Users():
+    pass
+
+@app.get(
+    path='/users/{user_id}',
+    status_code=status.HTTP_200_OK,
+    response_model=User,
+    summary='show_a_user',
+    tags=['Users']
+)
+def user():
+    pass
+
+@app.delete(
+    path='/users/delete/{user_id}',
+    status_code=status.HTTP_200_OK,
+    response_model=User,
+    summary='delete_user',
+    tags=['Users']
+)
+def delete():
+    pass
+
+@app.put(
+    path='/users/update/{user_id}',
+    status_code=status.HTTP_200_OK,
+    response_model=User,
+    summary='update_user',
+    tags=['Users']
+)
+def update():
+    pass
+
+
+##Twitter
