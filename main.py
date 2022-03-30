da978361615bf9ce8cf6f29fa14c4bbf6eb93a30
Jhory Toro <jhordytoro49@gmail.com>
2022-03-30 10:07:07 -0500

    'models_twittes'

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #python
-from datetime import date
+from datetime import date, datetime
 from uuid import UUID
 from typing import Optional
 
@@ -10,20 +10,28 @@ from pydantic import Field,EmailStr
 #FastAPI
 from fastapi import FastAPI
 
-class user_base(BaseModel):
+
+class User_Base(BaseModel):
     user_id: UUID = Field(...)
     email: EmailStr = Field(...)
-    
-class user_login(user_base):
-    password: str = Field(...,min_length=8)
-    
-class user(user_base):
+
+
+class User_Login(User_Base):
+    password: str = Field(...,min_length=8,max_length=64)
+
+
+class User(User_Base):
     first_name: str = Field(...,min_length=1,max_length=50)
     last_name: str = Field(...,min_length=1,max_length=50)
     birt_date: Optional[date] = Field(default=None)
 
+
 class twitter(BaseModel):
-    pass
+    twitte_id: UUID = Field(...)
+    contents: str = Field(...,min_length=1,max_length=264)
+    create_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
 
 app = FastAPI()
 
